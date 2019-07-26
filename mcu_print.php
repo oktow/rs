24035fd908b75cc8486648ca816c5edcba3d9bc9
oktow <38025387+oktow@users.noreply.github.com>
2019-07-27 00:58:31 +0700

    awal
    00

---
+++ mcu_print.php
@@ -0,0 +1,422 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>print mcu</title>
+</head>
+<body>
+
+<h1 style="text-align:center;"> <u>HASIL PEMERIKSAAN KESEHATAN</u></h1>
+<h3 style="text-align:center;">Nomor : 000/TU-3/DM/VII/2019</h1>
+
+<p><b>FORM IDENTITAS</b></p>
+<!-- *********************************************************************************************************************  -->
+<table width="100%" border="1">
+  <tr>
+  <td width="50%">Nama </td>
+  <td width="50%">Nama Nama Nama Nama Nama Nama Nama Nama </td>
+  </tr>
+
+   <tr>
+  <td width="50%">Jenis Kelamin </td>
+  <td width="50%">L / P</td>
+  </tr>
+
+  <tr>
+  <td width="50%">Kedudukan dalam keluarga</td>
+  <td width="50%">1.KK  2. Istri    3. Anak   4. Lain lain ..........</td>
+  </tr>
+
+
+  <tr>
+  <td width="50%">Agama</td>
+  <td width="50%">Islam</td>
+  </tr>
+
+  <tr>
+  <td width="50%">Alamat sesuai KTP</td>
+  <td width="50%">Alamat sesuai KTP Alamat sesuai KTP Alamat sesuai KTP</td>
+  </tr>
+
+
+  <tr>
+  <td width="50%">Status Perkawinan </td>
+  <td width="50%">1.Kawin  2. Tidak Kawin</td>
+  </tr>
+
+
+  <tr>
+  <td width="50%">Pekerjaan</td>
+  <td width="50%">Perusahaan : ................. Department : ................. Posisi: ................. Lama Kerja : .................</td>
+  </tr>
+
+
+  <tr>
+  <td width="50%">Pemeriksaan ke </td>
+  <td width="50%">Pertama / Berkala </td>
+  </tr>
+
+
+  <tr>
+  <td width="50%">No. Telp / Hp</td>
+  <td width="50%">..........</td>
+  </tr>
+</table>
+
+<p></p>
+<!-- *********************************************************************************************************************  -->
+<table  width="100%" border="1">
+  <tr>
+  <td style=text-align:center><b> JENIS PEMERIKSAAN</b></td>
+    </tr>
+</table>
+
+<p><b>PEMERIKSAAN FISIK</b></p>
+<!-- *********************************************************************************************************************  -->
+<!-- A.Tanda Vital -->
+
+<table  width="100%" border="1">
+  <tr>
+  <td width="2%" style=text-align:Left><b>A.</b></td>
+  <td width="20%" style=text-align:Left><b>Tanda Vital</b></td>
+  </tr>
+
+  <tr>
+  <td width="2%" style=text-align:Left>1.</td>
+  <td width="20%" style=text-align:Left>Tekanan darah</td>
+  <td width="2%" style=text-align:Left>:</td>
+  <td style=text-align:Left>isi tekanan darah mmHg</td>
+    </tr>
+
+   <tr>
+  <td width="2%" style=text-align:Left>2.</td>
+  <td width="20%" style=text-align:Left>Denyut nadi</td>
+  <td width="2%" style=text-align:Left>:</td>
+  <td style=text-align:Left>isi Denyut nadi x / menit , isi cukup, irama teratur</td>
+    </tr>
+
+      <tr>
+  <td width="2%" style=text-align:Left>3.</td>
+  <td width="20%" style=text-align:Left>Pernafasan </td>
+  <td width="2%" style=text-align:Left>:</td>
+  <td style=text-align:Left>isi Pernafasan  x / menit</td>
+    </tr>
+
+</table>
+<!-- *********************************************************************************************************************  -->
+<!-- B.Status Gizi -->
+
+<table  width="100%" border="1">
+  <tr>
+  <td width="2%" style=text-align:Left><b>B.</b></td>
+  <td width="20%" style=text-align:Left><b>Status Gizi</b></td>
+  </tr>
+
+  <tr>
+  <td width="2%" style=text-align:Left>1.</td>
+  <td width="20%" style=text-align:Left>Tinggi Badan</td>
+  <td width="2%" style=text-align:Left>:</td>
+  <td style=text-align:Left>isi Tinggi Badan cm</td>
+    </tr>
+
+   <tr>
+  <td width="2%" style=text-align:Left>2.</td>
+  <td width="20%" style=text-align:Left>Berat Badan</td>
+  <td width="2%" style=text-align:Left>:</td>
+  <td style=text-align:Left>isi Berat Badan kg</td>
+    </tr>
+
+      <tr>
+  <td width="2%" style=text-align:Left>3.</td>
+  <td width="20%" style=text-align:Left>Lingkar Perut	 </td>
+  <td width="2%" style=text-align:Left>:</td>
+  <td style=text-align:Left>isi Lingkar Perut cm</td>
+      </tr>
+
+      <tr>
+  <td width="2%" style=text-align:Left>4.</td>
+  <td width="20%" style=text-align:Left>IMT	 </td>
+  <td width="2%" style=text-align:Left>:</td>
+  <td style=text-align:Left>isi IMT</td>
+      </tr>
+</table>
+<!-- *********************************************************************************************************************  -->
+<!-- C.Tingkat Kesadaran -->
+
+<table  width="100%" border="1">
+  <tr>
+  <td width="2%" style=text-align:Left><b>C.</b></td>
+  <td width="20%" style=text-align:Left><b>Tingkat Kesadaran</b></td>
+  </tr>
+
+  <tr>
+  <td width="2%" style=text-align:Left>1.</td>
+  <td width="20%" style=text-align:Left>Kualitas Kontak</td>
+  <td width="2%" style=text-align:Left>:</td>
+  <td style=text-align:Left>isi Kualitas Kontak</td>
+    </tr>
+
+   <tr>
+  <td width="2%" style=text-align:Left>2.</td>
+  <td width="20%" style=text-align:Left>Kesadaran </td>
+  <td width="2%" style=text-align:Left>:</td>
+  <td style=text-align:Left>isi Kesadaran </td>
+    </tr>
+
+</table>
+<!-- *********************************************************************************************************************  -->
+<!-- D.Kelenjar Getah Bening -->
+
+<table  width="100%" border="1">
+  <tr>
+  <td width="2%" style=text-align:Left><b>D.</b></td>
+  <td width="20%" style=text-align:Left><b>Kelenjar Getah Bening</b></td>
+  </tr>
+
+  <tr>
+  <td width="2%" style=text-align:Left>1.</td>
+  <td width="20%" style=text-align:Left>Leher</td>
+  <td width="2%" style=text-align:Left>:</td>
+  <td style=text-align:Left>isi Leher</td>
+    </tr>
+
+   <tr>
+  <td width="2%" style=text-align:Left>2.</td>
+  <td width="20%" style=text-align:Left>Sub Mandibula </td>
+  <td width="2%" style=text-align:Left>:</td>
+  <td style=text-align:Left>isi Sub Mandibula </td>
+    </tr>
+
+      <tr>
+  <td width="2%" style=text-align:Left>3.</td>
+  <td width="20%" style=text-align:Left>Ketiak</td>
+  <td width="2%" style=text-align:Left>:</td>
+  <td style=text-align:Left>isi Ketiak</td>
+      </tr>
+
+</table>
+<!-- *********************************************************************************************************************  -->
+<!-- E.Kepala dan Leher -->
+
+<table  width="100%" border="1">
+  <tr>
+  <td width="2%" style=text-align:Left><b>E.</b></td>
+  <td width="20%" style=text-align:Left><b>Kepala dan Leher</b></td>
+  </tr>
+
+  <tr>
+  <td width="2%" style=text-align:Left>1.</td>
+  <td width="20%" style=text-align:Left>Tulang</td>
+  <td width="2%" style=text-align:Left>:</td>
+  <td style=text-align:Left>isi Tulang</td>
+    </tr>
+
+   <tr>
+  <td width="2%" style=text-align:Left>2.</td>
+  <td width="20%" style=text-align:Left>Kulit</td>
+  <td width="2%" style=text-align:Left>:</td>
+  <td style=text-align:Left>isi Kulit</td>
+    </tr>
+
+      <tr>
+  <td width="2%" style=text-align:Left>3.</td>
+  <td width="20%" style=text-align:Left>Bentuk Wajah</td>
+  <td width="2%" style=text-align:Left>:</td>
+  <td style=text-align:Left>isi Bentuk Wajah</td>
+      </tr>
+
+</table>
+<!-- *********************************************************************************************************************  -->
+<!-- F.Mata  -->
+
+<table  width="100%" border="1">
+  <tr>
+  <td width="2%" style=text-align:Left><b>F.</b></td>
+  <td width="20%" style=text-align:Left><b>Mata </b></td>
+  </tr>
+
+  <tr>
+  <td width="2%" style=text-align:Left>1.</td>
+  <td width="20%" style=text-align:Left>Jarak pandang</td>
+  <td width="2%" style=text-align:Left>:</td>
+  <td style=text-align:Left>isi Jarak pandang</td>
+    </tr>
+
+   <tr>
+  <td width="2%" style=text-align:Left>2.</td>
+  <td width="20%" style=text-align:Left>Visus Mata kanan (OD)</td>
+  <td width="2%" style=text-align:Left>:</td>
+  <td style=text-align:Left>isi Visus Mata kanan (OD)</td>
+    </tr>
+
+      <tr>
+  <td width="2%" style=text-align:Left>3.</td>
+  <td width="20%" style=text-align:Left>Visus Mata kiri (OS)</td>
+  <td width="2%" style=text-align:Left>:</td>
+  <td style=text-align:Left>isi Visus Mata kiri (OS)</td>
+      </tr>
+
+ <tr>
+  <td width="2%" style=text-align:Left>4.</td>
+  <td width="20%" style=text-align:Left>Lapang pandang</td>
+  <td width="2%" style=text-align:Left>:</td>
+  <td style=text-align:Left>isi Lapang pandang</td>
+      </tr>
+       <tr>
+  <td width="2%" style=text-align:Left>5.</td>
+  <td width="20%" style=text-align:Left>Buta warna</td>
+  <td width="2%" style=text-align:Left>:</td>
+  <td style=text-align:Left>isi Buta warna</td>
+      </tr>
+       <tr>
+  <td width="2%" style=text-align:Left>6.</td>
+  <td width="20%" style=text-align:Left>Konjungtiva</td>
+  <td width="2%" style=text-align:Left>:</td>
+  <td style=text-align:Left>isi Konjungtiva</td>
+      </tr>
+       <tr>
+  <td width="2%" style=text-align:Left>7.</td>
+  <td width="20%" style=text-align:Left>Sklera</td>
+  <td width="2%" style=text-align:Left>:</td>
+  <td style=text-align:Left>isi Sklera</td>
+      </tr>
+       <tr>
+  <td width="2%" style=text-align:Left>8.</td>
+  <td width="20%" style=text-align:Left>Lensa Mata</td>
+  <td width="2%" style=text-align:Left>:</td>
+  <td style=text-align:Left>isi Lensa Mata</td>
+      </tr>
+       <tr>
+  <td width="2%" style=text-align:Left>9.</td>
+  <td width="20%" style=text-align:Left>Gerak Bola Mata</td>
+  <td width="2%" style=text-align:Left>:</td>
+  <td style=text-align:Left>isi Gerak Bola Mata</td>
+      </tr>
+  <tr>
+  <td width="2%" style=text-align:Left>10.</td>
+  <td width="20%" style=text-align:Left>Lain - lain</td>
+  <td width="2%" style=text-align:Left>:</td>
+  <td style=text-align:Left>isi lain - lain</td>
+      </tr>
+</table>
+
+<!-- *********************************************************************************************************************  -->
+<style type="text/css">
+.tg  {border-collapse:collapse;border-spacing:0;}
+.tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:black;}
+.tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:black;}
+.tg .tg-73a0{font-size:12px;border-color:inherit;text-align:left;vertical-align:top}
+.tg .tg-0pky{border-color:inherit;text-align:left;vertical-align:top}
+</style>
+<table class="tg" style="undefined;table-layout: fixed; width: 832px">
+<colgroup>
+<col style="width: 32px">
+<col style="width: 35px">
+<col style="width: 215px">
+<col style="width: 21px">
+<col style="width: 529px">
+</colgroup>
+  <tr>
+    <th class="tg-73a0" colspan="3"><span style="font-weight:bold">G. </span><span style="font-weight:700">Telinga Hidung dan Tenggorokan</span></th>
+    <th class="tg-0pky"></th>
+    <th class="tg-0pky"></th>
+  </tr>
+  <tr>
+    <td class="tg-73a0">1.</td>
+    <td class="tg-73a0" colspan="2">Telinga<br></td>
+    <td class="tg-0pky"></td>
+    <td class="tg-0pky"></td>
+  </tr>
+  <tr>
+    <td class="tg-0pky"></td>
+    <td class="tg-0pky">a.</td>
+    <td class="tg-0pky">Daun Telinga</td>
+    <td class="tg-0pky">:</td>
+    <td class="tg-0pky">Normal</td>
+  </tr>
+  <tr>
+    <td class="tg-0pky"></td>
+    <td class="tg-0pky">b.</td>
+    <td class="tg-0pky">Liang Telinga</td>
+    <td class="tg-0pky">:</td>
+    <td class="tg-0pky"> Normal, Cerumen (-)<br></td>
+  </tr>
+  <tr>
+    <td class="tg-0pky"></td>
+    <td class="tg-0pky">c.</td>
+    <td class="tg-0pky">Membrana Timpani</td>
+    <td class="tg-0pky">:</td>
+    <td class="tg-0pky">Intak</td>
+  </tr>
+  <tr>
+    <td class="tg-0pky"></td>
+    <td class="tg-0pky">d.</td>
+    <td class="tg-0pky">Test Garputala</td>
+    <td class="tg-0pky"></td>
+    <td class="tg-0pky"></td>
+  </tr>
+  <tr>
+    <td class="tg-0pky"></td>
+    <td class="tg-0pky"></td>
+    <td class="tg-0pky">- Test Webber</td>
+    <td class="tg-0pky">:</td>
+    <td class="tg-0pky">Normal</td>
+  </tr>
+  <tr>
+    <td class="tg-0pky"></td>
+    <td class="tg-0pky"></td>
+    <td class="tg-0pky">- Test Swabach</td>
+    <td class="tg-0pky">:</td>
+    <td class="tg-0pky">Normal</td>
+  </tr>
+  <tr>
+    <td class="tg-0pky"></td>
+    <td class="tg-0pky"></td>
+    <td class="tg-0pky">- Test Rinne</td>
+    <td class="tg-0pky">:</td>
+    <td class="tg-0pky">Normal</td>
+  </tr>
+  <tr>
+    <td class="tg-0pky"></td>
+    <td class="tg-0pky"></td>
+    <td class="tg-0pky">- Test Bing</td>
+    <td class="tg-0pky">:</td>
+    <td class="tg-0pky">Normal</td>
+  </tr>
+  <tr>
+    <td class="tg-0pky">1.</td>
+    <td class="tg-0pky" colspan="2">Telinga</td>
+    <td class="tg-0pky"></td>
+    <td class="tg-0pky"></td>
+  </tr>
+  <tr>
+    <td class="tg-0pky"></td>
+    <td class="tg-0pky"></td>
+    <td class="tg-0pky">- Meatus Nasi</td>
+    <td class="tg-0pky">:</td>
+    <td class="tg-0pky">Normal</td>
+  </tr>
+  <tr>
+    <td class="tg-0pky"></td>
+    <td class="tg-0pky"></td>
+    <td class="tg-0pky">- Septum Nasi</td>
+    <td class="tg-0pky">:</td>
+    <td class="tg-0pky">Normal</td>
+  </tr>
+  <tr>
+    <td class="tg-0pky"></td>
+    <td class="tg-0pky"></td>
+    <td class="tg-0pky">- Konka Nasal</td>
+    <td class="tg-0pky">:</td>
+    <td class="tg-0pky">Normal</td>
+  </tr>
+  <tr>
+    <td class="tg-0pky"></td>
+    <td class="tg-0pky"></td>
+    <td class="tg-0pky">- Nyeri Ketok</td>
+    <td class="tg-0pky">:</td>
+    <td class="tg-0pky">Normal</td>
+  </tr>
+</table>>
+
+</body>
+</html>
